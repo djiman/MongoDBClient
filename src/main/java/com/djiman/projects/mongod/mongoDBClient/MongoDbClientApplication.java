11df5f65c34a85df5ac1be1b77a64673764f2649
Initial Commit <gorguindong@hotmail.fr>
2020-06-25 22:39:50 +0200

    initial commit

---
+++ src/main/java/com/djiman/projects/mongod/mongoDBClient/MongoDbClientApplication.java
@@ -0,0 +1,13 @@
+package com.djiman.projects.mongod.mongoDBClient;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MongoDbClientApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MongoDbClientApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/djiman/projects/mongod/mongoDBClient/MongoDbClientApplicationTests.java
@@ -0,0 +1,13 @@
+package com.djiman.projects.mongod.mongoDBClient;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MongoDbClientApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
